329f6cfdb28ee6135f43bf20a08bdb2f5c7ed625
Tamas 4d <tamas.szucs@4dpipeline.com>
2014-05-20 14:09:48 +0200

    updated read me, added default build action, default build tool, gradle build added - still has issues!

--- README.md
+++ README.md
@@ -2,24 +2,64 @@ steps-eclipse-builder
 ==================================
 
 # Input Environment Variables
-- CONCRETE_PROJECT_PATH
-- CONCRETE_SCHEME
-- CONCRETE_ACTION_BUILD or CONCRETE_ACTION_ANALYZE or CONCRETE_ACTION_ARCHIVE
-- CONCRETE_CERTIFICATE_PASSPHRASE
-- CONCRETE_CERTIFICATE_URL
-- CONCRETE_PROVISION_URL
-- CONCRETE_BUILD_TOOL: "xcodebuild" is the default; "xctool" is supported; CONCRETE_ARCHIVE_STATUS can only use "xcodebuild"
+- CONCRETE_SOURCE_DIR
+- CONCRETE_ACTION=[BUILD/DEBUG]
+- CONCRETE_BUILD_TOOL: "ant" is the default; "gradle" is supported
 
 # Output Environment Variables
 (accessible for Steps running after this Step)
 
-## if CONCRETE_ACTION_BUILD
+## if CONCRETE_ACTION == BUILD
 - CONCRETE_BUILD_STATUS=[success/failed] 
 
-## if CONCRETE_ACTION_ANALYZE
-- CONCRETE_ANALYZE_STATUS=[success/failed]
+## if CONCRETE_ACTION == DEBUG
+- CONCRETE_DEBUG_STATUS=[success/failed]
 
-## if CONCRETE_ACTION_ARCHIVE
-- CONCRETE_ARCHIVE_STATUS=[success/failed]
-- CONCRETE_IPA_PATH
-- CONCRETE_DSYM_PATH
+
+
+
+Needed for android build
+
+# http://zeroturnaround.com/rebellabs/java-build-tools-maven-gradle-and-ant-plus-the-dsl-vs-xml-debate/
+
+——— ANT BUILD ———
+
+.bash_profile-ba:
+
+export JAVA_HOME=$(/usr/libexec/java_home)
+export ANT_HOME=/usr/local/ant
+export PATH=${PATH}:${ANT_HOME}/bin
+export ANDROID_HOME=/Users/dev/adt/sdk
+export PATH=${PATH}:${ANDROID_HOME}/tools
+export GRADLE_HOME=/Users/dev/tools/gradle
+export PATH=${PATH}:${GRADLE_HOME}/bin
+
+Projektnel:
+target megfelelo beallitasa v az elvart SDK-nak fenn kell lennie
+build.xml kell hozza - ANT-hoz
+
+
+Reference libraries http://blog.standalonecode.com/?p=269
+
+Analyze alapbol nincs benne, Sonar Ant Task-kal lehet
+http://frommyworkshop.blogspot.hu/2011/04/analyse-with-ant-sonar-way.html
+
+
+adt
+ant tool
+
+
+———— GRADLE BUILD ——————
+
+gradle required http://www.gradle.org/downloads :)
+
+http://www.vogella.com/tutorials/AndroidBuild/article.html
+
+
+
+
+— — APP SIGNING ———
+keytool
+http://developer.android.com/tools/publishing/app-signing.html
+
+-- NDK build : http://stackoverflow.com/questions/7432449/android-ndk-build-with-ant-script
--- step.sh
+++ step.sh
@@ -2,37 +2,18 @@
 # @Author: Tamas Szucs
 # @Date:   2014-05-19 10:39:46
 # @Last Modified by:   Tamas Szucs
-# @Last Modified time: 2014-05-19 17:49:07
+# @Last Modified time: 2014-05-20 14:08:42
 
 echo "$ cd $CONCRETE_SOURCE_DIR"
 
 cd "$CONCRETE_SOURCE_DIR"
 
 
-# TODO branch ant and gradle builds - maybe in a separate step
-# default should be ant with 'android update ....' command
-
-ant clean
-# TODO check if androidmanifest name can change?
-sdk_ver="android-$(grep -ro '<uses-sdk[ \t].*android:targetSdkVersion="[^"]*"' AndroidManifest.xml \
-	| grep -o 'android:targetSdkVersion="[^"]*"' | cut -f2 -d'"' )"
-
-echo "SDK VER"
-echo $sdk_ver
-
-# --target 1 is the id from 'android list targets'
-android update project --path . --target 
-
-# TODO reference other libraries http://blog.standalonecode.com/?p=269
-
-
-
-
-# default action == BUILD
-echo $CONCRETE_ACTION
+# default action = BUILD
 if [[ ! $CONCRETE_ACTION ]]; then
 	CONCRETE_ACTION="BUILD"
 fi
+echo $CONCRETE_ACTION
 
 # upgraded - passed as parameter, check against its value instead 
 # of setting multiple env vars
@@ -43,19 +24,85 @@ fi
 #	ANALYZE(also todo, no built-in support)
 echo "CONCRETE_ACTION: $CONCRETE_ACTION" 
 
+# default build tool = ant
+if [[ ! $CONCRETE_BUILD_TOOL ]]; then
+	CONCRETE_BUILD_TOOL="ant"
+fi
+echo "CONCRETE_BUILD_TOOL: $CONCRETE_BUILD_TOOL"
 
+
+# ant initializations
+if [[ $CONCRETE_BUILD_TOOL == "ant" ]]; then
+	echo "Cleaning first...."
+	ant clean
+
+	# TODO check if AndroidManifest.xml file name is constant?
+	sdk_ver="android-$(grep -o 'android:targetSdkVersion="[^"]*"' AndroidManifest.xml | cut -f2 -d'"' )"
+
+	echo "SDK version=$sdk_ver"
+
+	creates build.xml and local.properties required by ant
+	android update project --path . --target $sdk_ver
+	update_result=$?
+
+	# if sdk is not installed install it
+	if [ $update_result -ne 0 ]; then
+		echo "The target sdk is not installed, downloading and installing now...."
+		echo yes | android update sdk -u -a --filter $sdk_ver
+		android update project --path . --target $sdk_ver
+	fi
+
+	# TODO reference other libraries http://blog.standalonecode.com/?p=269
+
+fi
+
+#################
+##  Building...
+#################
 
 if [[ $CONCRETE_ACTION == "BUILD" ]]; then
 	# if only build
-	ant
+	if [[ $CONCRETE_BUILD_TOOL == "ant" ]]; then
+		ant
+	else
+		gradle clean build
+	fi
+	
+	action_result=$?
+
+	if [ $action_result -ne 0 ]; then
+		echo "export CONCRETE_BUILD_STATUS=\"failed\"" >> ~/.bash_profile
+		echo "CONCRETE_BUILD_STATUS=\"failed\""
+	else
+		echo "export CONCRETE_BUILD_STATUS=\"success\"" >> ~/.bash_profile
+		echo "CONCRETE_BUILD_STATUS=\"success\""
+	fi
+
+
 elif [[ $CONCRETE_ACTION == "DEBUG" ]]; then
 	# creating aligned and debug signed apk
-	ant debug
+	if [[ $CONCRETE_BUILD_TOOL == "ant" ]]; then
+		ant debug
+	else
+		gradle clean build
+	fi
+
+	action_result=$?
+
+	if [ $action_result -ne 0 ]; then
+		echo "export CONCRETE_DEBUG_STATUS=\"failed\"" >> ~/.bash_profile
+		echo "CONCRETE_BUILD_STATUS=\"failed\""
+	else
+		echo "export CONCRETE_DEBUG_STATUS=\"success\"" >> ~/.bash_profile
+		echo "CONCRETE_BUILD_STATUS=\"success\""
+	fi
+
 elif [[ $CONCRETE_ACTION == "RELEASE" ]]; then
 	# TODO
 	# create unsigned and unaligned APK, need to be aligned and signed manually
 	echo "Action currently not supported! Terminating..."
 	exit 1
+
 elif [[ $CONCRETE_ACTION == "ANALYZE" ]]; then
 	# TODO
 	# supported only by using external tool. maybe another step
@@ -63,3 +110,4 @@ elif [[ $CONCRETE_ACTION == "ANALYZE" ]]; then
 	exit 1
 fi
 
+exit $action_result
